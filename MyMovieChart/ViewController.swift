998ce51bde96442fcae4de38d9a0d4ad0ba838e3
heyask <sunrivs@gmail.com>
2016-04-27 01:49:15 +0900

    Initial Commit

---
+++ MyMovieChart/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyMovieChart
+//
+//  Created by 김승현 on 2016. 4. 27..
+//  Copyright © 2016년 aivesoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
